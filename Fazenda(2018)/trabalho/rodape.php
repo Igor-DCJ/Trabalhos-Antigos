a65eefdb3e8f1bea30caeaa8d38ebbab27b4f966
Igor-DCJ <diascostaigor@gmail.com>
2022-03-16 20:37:45 -0300

    Create rodape.php

---
+++ Fazenda(2018)/trabalho/rodape.php
@@ -0,0 +1,19 @@
+<!--
+    <footer class="footer text-faded text-center py-5">
+      <div class="container">
+        <p class="m-0 small">Copyright &copy; Your Website 2018</p>
+      </div>
+    </footer>
+
+     Bootstrap core JavaScript --
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+
+    por alguma razão o git fala que aqui tem hack,  vai entender....
+
+-->
+
+  </body>
+
+</html>
